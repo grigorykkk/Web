5aa6251a96e97ac7eed1daab64a7c1d1ae6b26ed
grigorykkk <145666916+grigorykkk@users.noreply.github.com>
2026-02-27 08:15:53 +0000

    fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://stunning-couscous-7v9jx5jp44v72xgr9-3000.app.github.dev/',
   headers: {
     'Content-Type': 'application/json',
     'accept': 'application/json',
--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ let products = [
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
